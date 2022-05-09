79c1c8e614cf61dc2ca57f31c2b1e15780da75d0
Gorbushka3000 <glebdeshar@gmail.com>
2022-05-09 15:10:23 +0300

    Сущности

---
+++ Model/Users.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EloctrnicJournal_EF.Model
+{
+    public class Users
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? LastName { get; set; }
+        [Required]
+        public string? Email { get; set; }   
+        [Required]
+        public int PhoneNumber { get; set; }
+        [Required]
+        public DateTime BirtDay { get; set; }
+    }
+
+    public class Students : Users
+    {
+        public int Id { get; set; }
+        public int Class { get; set; }
+        public Parents Parents { get; set; }
+        public int ParentsId { get; set; }
+    }
+    public class Parents : Users
+    {
+        public int Id { get; set; }
+        public List<Students> Students { get; set; }
+    }
+    public class Teachers : Users
+    {
+        public int Id { get; set; }
+        public List<Students> Students { get; set; }
+    }
+}
