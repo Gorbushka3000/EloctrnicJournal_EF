9cb788f4ededca47810b5c76a7522e3811fc3b82
Gorbushka3000 <glebdeshar@gmail.com>
2022-05-09 16:47:06 +0300

    Контекст

---
+++ Data/EJContext.cs
@@ -0,0 +1,38 @@
+﻿using EloctrnicJournal_EF.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace EloctrnicJournal_EF.Data
+{
+    public class EJContext : DbContext
+    {
+        public static EJContext context;
+        public DbSet<Students> Students => Set<Students>();
+        public DbSet<Parents> Parents => Set<Parents>();
+        public DbSet<Teachers> Teacher => Set<Teachers>();
+
+        public EJContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        public static EJContext GetContext()
+        {
+            if (context == null)
+                context = new EJContext();
+            return context;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=ElectronicJournal.db"); ;
+            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Parents>();
+            modelBuilder.Entity<Teachers>();
+            modelBuilder.Entity<Students>();
+        }
+    }
+}
